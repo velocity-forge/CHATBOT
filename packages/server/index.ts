1ed7f2044dc55f0e003dfeb73d5b21d2cfb03689
velocity-forge <velforge.tech28@gmail.com>
2025-10-30 23:02:55 -0400

    Set up router file

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,23 +1,13 @@
 import express from 'express';
-import type { Request, Response } from 'express';
 import dotenv from 'dotenv';
-import { chatController } from './controllers/chat.controller';
-
+import router from './router';
 dotenv.config();
 
 const app = express();
 app.use(express.json());
+app.use(router);
+
 const port = process.env.PORT || 3000;
-
-app.get('/', (req: Request, res: Response) => {
-   res.send('Hello World!');
-});
-
-app.get('/api/hello', (req: Request, res: Response) => {
-   res.json({ message: 'Hello World!' });
-});
-
-app.post('/api/chat', chatController.sendMessage);
 
 app.listen(port, () => {
    console.log(`Server is running on port http://localhost:${port}`);
