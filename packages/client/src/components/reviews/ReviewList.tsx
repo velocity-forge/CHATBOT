a84cc7f2f59f095786b89740a9491e9551f8dd4a
velocity-forge <velforge.tech28@gmail.com>
2025-11-27 16:38:23 -0500

    Displaying Loading Skeletons

--- packages/client/src/components/reviews/ReviewList.tsx
+++ packages/client/src/components/reviews/ReviewList.tsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import StarRating from './StarRating';
+import Skeleton from 'react-loading-skeleton';
 
 type Props = {
    productId: number;
@@ -21,17 +22,34 @@ type GetReviewsResponse = {
 
 const ReviewList = ({ productId }: Props) => {
    const [reviewData, setReviewData] = useState<GetReviewsResponse>();
+   const [isLoading, setIsLoading] = useState(false);
 
    const fetchReviews = async () => {
+      setIsLoading(true);
       const { data } = await axios.get<GetReviewsResponse>(
          `/api/products/${productId}/reviews`
       );
       setReviewData(data);
+      setIsLoading(false);
    };
 
    useEffect(() => {
       fetchReviews();
    }, [productId]);
+
+   if (isLoading) {
+      return (
+         <div className="flex flex-col gap-5">
+            {[1, 2, 3].map((p) => (
+               <div key={p}>
+                  <Skeleton width={150} />
+                  <Skeleton width={100} />
+                  <Skeleton count={2} />
+               </div>
+            ))}
+         </div>
+      );
+   }
 
    return (
       <div className="flex flex-col gap-5">
